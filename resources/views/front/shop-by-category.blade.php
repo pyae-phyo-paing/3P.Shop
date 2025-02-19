5ea66ee924349af00f46f1f9a443ce93af97bd8c
pyae-phyo-paing <paing.mdy.edu@gmail.com>
2025-02-19 16:21:56 +0630

    3P.Shop Project Modefied first time for Feb 19,2025

--- resources/views/front/shop-by-category.blade.php
+++ resources/views/front/shop-by-category.blade.php
@@ -170,16 +170,16 @@
                                     <div class="carousel-item">
                                         <div class="row">
                                             <div class="col-3 p-md-5">
-                                                <a href="#"><img class="img-fluid brand-img" src="{{asset('front-assets/assets/img/brand_01.png')}}" alt="Brand Logo"></a>
+                                                <a href="#"><img class="img-fluid brand-img" src="{{asset('front-assets/assets/img/ysl_logo.png')}}" alt="Brand Logo"></a>
                                             </div>
                                             <div class="col-3 p-md-5">
-                                                <a href="#"><img class="img-fluid brand-img" src="{{asset('front-assets/assets/img/brand_02.png')}}" alt="Brand Logo"></a>
+                                                <a href="#"><img class="img-fluid brand-img" src="{{asset('front-assets/assets/img/dior_logo.png')}}" alt="Brand Logo"></a>
                                             </div>
                                             <div class="col-3 p-md-5">
-                                                <a href="#"><img class="img-fluid brand-img" src="{{asset('front-assets/assets/img/brand_03.png')}}" alt="Brand Logo"></a>
+                                                <a href="#"><img class="img-fluid brand-img" src="{{asset('front-assets/assets/img/louis_vuitton_logo.png')}}" alt="Brand Logo"></a>
                                             </div>
                                             <div class="col-3 p-md-5">
-                                                <a href="#"><img class="img-fluid brand-img" src="{{asset('front-assets/assets/img/brand_04.png')}}" alt="Brand Logo"></a>
+                                                <a href="#"><img class="img-fluid brand-img" src="{{asset('front-assets/assets/img/gucci_logo.png')}}" alt="Brand Logo"></a>
                                             </div>
                                         </div>
                                     </div>
@@ -189,7 +189,7 @@
                                     <div class="carousel-item">
                                         <div class="row">
                                             <div class="col-3 p-md-5">
-                                                <a href="#"><img class="img-fluid brand-img" src="{{asset('front-assets/assets/img/brand_01.png')}}" alt="Brand Logo"></a>
+                                                <a href="#"><img class="img-fluid brand-img" src="{{asset('front-assets/assets/img/calvin_klein_logo.png')}}" alt="Brand Logo"></a>
                                             </div>
                                             <div class="col-3 p-md-5">
                                                 <a href="#"><img class="img-fluid brand-img" src="{{asset('front-assets/assets/img/brand_02.png')}}" alt="Brand Logo"></a>
